5a07e88abfa9cba1569f967e0912847b70d0c3b3
muhammadsyafiqakmal06 <67745635+muhammadsyafiqakmal06@users.noreply.github.com>
2024-01-01 21:16:06 +0700

    Muhammad Naufal Akbar 2200018291

---
+++ src/Projek_kasir.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projek_kasir;
+
+/**
+ *
+ * @author User
+ */
+public class Projek_kasir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         new form_utama().show();
+    }
+    
+}
